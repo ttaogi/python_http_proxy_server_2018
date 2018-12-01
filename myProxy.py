5dd60a80568da2e4b88f54490bd9346a3e82cf2d
ttaogi <linian0kr@gmail.com>
2018-12-01 14:07:12 -0800

    1st commit

---
+++ myProxy.py
@@ -0,0 +1,159 @@
+import socket, sys
+#from thread import *
+from threading import *
+
+listening_port = 4000
+max_conn = 5
+buffer_size = 4096
+
+##########
+##########
+##########
+
+def start():
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        print(":::initialize socket.")
+    except Exception as e:
+        print(":::initialize error.")
+        print(e)
+        sys.exit(1)
+
+    try:
+        sa = socket.getaddrinfo("127.0.0.1", 4000, socket.AF_INET, socket.SOCK_STREAM)[0]
+        print(sa)
+        #s.bind(('', listening_port))
+        #s.bind((socket.gethostname, listening_port))
+        s.bind(sa[4])
+        print(":::bind socket.")
+    except Exception as e:
+        print(":::bind error.")
+        print(e)
+        sys.exit(1)
+
+    try:
+        s.listen(max_conn)
+        print(":::start server.")
+    except Exception as e:
+        print(":::listen error.")
+        print(":::listening port num : %d."%(listening_port))
+        print(e)
+        sys.exit(1)
+
+    while 1:
+        try:
+            conn, addr = s.accept()
+            data = conn.recv(buffer_size)
+            #start_new_thread(conn_string, (conn, data, addr))
+            new_thread = Thread(target=conn_string, args=(conn, data, addr))
+            new_thread.start()
+        except KeyboardInterrupt as e:
+            s.close()
+            print(":::Terminate proxy server.")
+            sys.exit(1)
+
+    s.close()
+
+def conn_string(conn, data, addr):
+    print(":::call conn_string")
+    webserver = ''
+    port = 80
+
+    try:
+        str_data = data.decode('utf-8')
+        first_line = str_data.split('\n')[0]
+        print("first line:")
+        print(first_line)
+        url = first_line.split(' ')[1]
+        print("url:")
+        print(url)
+        http_pos = url.find("://")
+
+        if(http_pos == -1):
+            temp = url
+        else:
+            temp = url[(http_pos+3):]
+
+        port_pos = temp.find(":")
+        webserver_pos = temp.find("/")
+        if(webserver_pos == -1):
+            webserver_pos = len(temp)
+
+        webserver = ""
+        port = -1
+
+        if(port_pos == -1 or webserver_pos < port_pos):
+            port = 80
+            webserver = temp[:webserver_pos]
+        else:
+            port = int((temp[(port_pos+1):])[:webserver_pos - port_pos-1])
+            webserver = temp[:port_pos]
+        print("port:")
+        print(port)
+        print("webserver:")
+        print(webserver)
+    except Exception as e:
+        print(":::conn_string error-1")
+        print(e)
+        return
+
+    try:
+        proxy_server(webserver, port, conn, data, addr)
+    except Exception as e:
+        print(":::conn_string error-2")
+        print(e)
+        pass
+
+def proxy_server(webserver, port, conn, data, addr):
+    print(":::call proxy_server")
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        print("&&&&")
+        s.connect((webserver, port))
+        print("****")
+        print("data type:")
+        print(type(data))
+        print("data:")
+        print(data)
+        s.send(data)
+
+        print("%%%%")
+        while 1:
+            reply = s.recv(buffer_size)
+
+            print(":::proxy_sever reply:")
+            print(reply)
+
+            if(len(reply) > 0):
+                conn.send(reply)
+                dar = float(len(reply))
+                dar = float(dar/1024)
+                dar = "%.3s"%(str(dar))
+                dar = "%s KB"%(dar)
+                print(":::Request done : %s = %s."%(str(addr[0]), str(dar)))
+            else:
+                break
+
+        print(":::proxy_server end")
+        s.close()
+        conn.close()
+    except Exception as e:
+        print(":::proxy_server error")
+        print(e)
+        s.close()
+        conn.close()
+        sys.exit(1)
+        
+##########
+##########
+##########
+
+try:
+    listening_port = int(input(":::Enter listening port number : "))
+except KeyboardInterrupt:
+    print(":::User requested an interrupt.")
+    sys.exit()
+
+start()
+
+
---
+++ myProxy_old_00.py
@@ -0,0 +1,89 @@
+import socket
+import select
+import time
+import sys
+
+buffer_size = 4096
+delay = 0.0001
+forward_to = ("smtp.zaz.ufsk.br", 25)
+
+class Forward:
+    def __init__(self):
+        self.forward = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    def start(self, host, port):
+        try:
+            self.forward.connect((host, port))
+        except Exception as e:
+            print(e)
+            return False
+
+class ProxyServer:
+    input_list = []
+    channel = {}
+
+    def __init__(self, host, port):
+        self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.server.bind((host, port))
+        self.server.listen(200)
+
+    def main_loop(self):
+        self.input_list.append(self.server)
+        while 1:
+            time.sleep(delay)
+            ss = select.select
+            inputready, outputready, exceptready = ss(self.input_list, [], [])
+            for self.s in inputready:
+                if self.s == self.server:
+                    self.on_accept()
+                    break
+                self.data = self.s.recv(buffer_size)
+                if len(self.data) == 0:
+                    self.on_close()
+                    break
+                else:
+                    self.on_recv()
+
+    def on_accept(self):
+        forward = Forward().start(forward_to[0], forward_to[1])
+        clientsock, clientaddr = self.server.accept()
+        if forward:
+            print(clientaddr)
+            print(" has connected")
+            self.input_list.append(clientsock)
+            self.input_list.append(forward)
+            self.channel[clientsock] = forward
+            self.channel[forward] = clientsock
+        else:
+            print("Can`t establich connection with remote server.")
+            print("Closing connection with client side.")
+            print(clientaddr)
+            clientsock.close()
+
+    def on_close(self):
+        print(self.s.getpeername())
+        print(" has disconnected.")
+
+        self.inpout_list.remove(self.s)
+        self.input_list.remove(self.channel[self.s])
+        out = self.channel[self.s]
+
+        self.channel[out].close()
+        self.channel[self.s].close()
+
+        del self.channel[out]
+        del self.channel[self.s]
+
+    def on_recv(self):
+        data = self.data
+        print(data)
+        self.channel[self.s].send(data)
+
+if __name__ == "__main__":
+    server = ProxyServer('', 4000)
+    try:
+        server.main_loop()
+    except KeyboardInterrupt as ki:
+        print("input ctrl + c")
+        sys.exit(1)
